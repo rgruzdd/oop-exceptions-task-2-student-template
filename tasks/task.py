2dd50864222d0ef13c5bc6d2f2cb0147fe8c11ab
Gruzin <gruzd.roman@inbox.ru>
2023-04-19 21:31:21 +0300

    Commit

--- tasks/task.py
+++ tasks/task.py
@@ -3,21 +3,18 @@ from typing import Union
 
 def divide(str_with_ints: str) -> Union[float, str]:
     num = []
-
     for x in str_with_ints.split():
         try:
             num.append(int(x))
         except ValueError:
-            print('Error code: invalid literal for int() with base 10: ' + "'" + x + "'")
-            return
+            return 'Error code: invalid literal for int() with base 10: ' + "'" + x + "'"
+
     try:
         num_list = num[0] / num[1]
         return float(num_list)
 
     except ZeroDivisionError:
-        print('Error code: division by zero')
+        return 'Error code: division by zero'
 
-
-
-
+print(divide("4 0"))
 
